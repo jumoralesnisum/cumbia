b2e04154d7547c9d40fc85cae58f50dec6147bac
Juan Pablo Morales <juan.morales@clickatell.com>
2023-06-06 10:37:19 -0500

    remove spanishhalf-built

--- main.py
+++ main.py
@@ -57,9 +57,6 @@ Category:
 '''.format(text), "us-central1")
     return jsonify({"result": "success", "value": result, "text": text})
 
-@app.route("/predecir", methods=['GET', 'POST'])
-def predecir():
-    
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
